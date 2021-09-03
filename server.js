89211b8dbb09413d2f450cccd825cb0ca95e33e3
Cayman Gill <cayman.g@hotmail.com>
2021-09-02 21:39:42 -0400

    added default page for application

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('develop/public'));
+app.use(express.static('Develop/public'));
 
 app.get('/api/notes', (req, res) => {
     res.json(noteData);
@@ -42,6 +42,11 @@ app.delete('/api/notes/:id', (req, res) => {
         }
     };
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
+});
+
 
 app.listen(PORT, () => {
     console.log('Server Started');
